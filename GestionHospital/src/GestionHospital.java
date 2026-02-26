5330a549bf090b5bcd2e9145d6a1949eea523202
Victor13578 <reyescardonavictormanuel333@gmail.com>
2026-02-26 10:49:44 -0500

    Correct Documentation
    Documentaacion de la clase

--- GestionHospital/src/GestionHospital.java
+++ GestionHospital/src/GestionHospital.java
@@ -1,40 +1,83 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import java.util.ArrayList;
 
+/**
+ * Clase que gestiona el funcionamiento del hospital.
+ * Implementa la interfaz IHospital para iniciar la simulación del sistema.
+ * 
+ * @author Victor, Juan Sebastian & Sergio Arango 
+ * @version 1.0
+ */
 public class GestionHospital implements IHospital {
+
+    /**
+     * Método principal que inicia el día en el hospital.
+     * Se crean médicos, cirujanos y pacientes y se ejecutan
+     * las acciones principales del sistema.
+     */
+    @Override
     public void iniciarDia() {
-        Medico m1 = new Medico("Carlos Perez", "1", 40, "M", "Cardiologia", 101);
-        Cirujano c1 = new Cirujano("Luis Mora", "3", 50, "M", "Cirugia", 201, 5, true);
-        Paciente p1 = new Paciente("Juan", "11", 20, "M", "H1", "Sura", (double) 100.0F, "infarto");
+
+        // crear medico
+        Medico m1 = new Medico("Carlos Perez", "1", 40, "M",
+                "Cardiologia", 101);
+
+        // crear cirujano
+        Cirujano c1 = new Cirujano("Luis Mora", "3", 50, "M",
+                "Cirugia", 201, 5, true);
+
+        // crear paciente
+        Paciente p1 = new Paciente("Juan", "11", 20, "M",
+                "H1", "Sura", 100.0, "infarto");
+
+        // asignar paciente al medico
         m1.agregarPaciente(p1);
-        ArrayList<Persona> personas = new ArrayList();
+
+        // lista polimorfica
+        ArrayList<Persona> personas = new ArrayList<>();
         personas.add(m1);
         personas.add(c1);
         personas.add(p1);
 
+        System.out.println("\n--- PRESENTACION ---");
+
+        // polimorfismo
         for (Persona per : personas) {
             per.mostrarDatos();
         }
 
+        // medico revisa pacientes
         m1.revisarPacientes();
+
+        // prioridad
         String prioridad = m1.asignarPrioridad(p1.getSintoma());
-        System.out.println("Prioridad: " + prioridad);
-        p1.pagarConsulta((double) 50.0F);
+        System.out.println("Prioridad asignada: " + prioridad);
+
+        // paciente intenta pagar
+        p1.pagarConsulta(50.0);
+
+        // cirujano opera
         c1.realizarOperacion();
-        this.procesarIngreso(p1);
+
+        // gestion ingreso
+        procesarIngreso(p1);
     }
 
+    /**
+     * Procesa el ingreso del paciente al hospital
+     * y lo envía a pabellón según su EPS.
+     * 
+     * @param p paciente que ingresa
+     */
     public void procesarIngreso(Paciente p) {
-        System.out.println("\nAtención paciente " + p.getNombre());
-        if (p.getEps().equalsIgnoreCase("Sura")) {
-            System.out.println("Pabellón A");
-        } else {
-            System.out.println("Pabellón General");
-        }
 
+        System.out.println("\nAtención paciente " + p.getNombre());
+
+        if (p.getEps().equalsIgnoreCase("Sura")) {
+            System.out.println("Enviado a Pabellón A");
+        } else if (p.getEps().equalsIgnoreCase("Sanitas")) {
+            System.out.println("Enviado a Pabellón B");
+        } else {
+            System.out.println("Enviado a Pabellón General");
+        }
     }
 }
