a6bd5eb712482e1eed5480775f7b5d7d83092015
Victor13578 <reyescardonavictormanuel333@gmail.com>
2026-02-26 10:52:20 -0500

    Documentacion Clase
    doc

--- GestionHospital/src/Persona.java
+++ GestionHospital/src/Persona.java
@@ -1,61 +1,42 @@
 /**
- * Constructor de la clase Persona.
- * * @author Juan Sebastian y victor
+ * Clase abstracta que representa una persona del hospital.
+ * Es la clase base para Medico y Paciente.
  * 
+ * @author Victor
  * @version 1.0
  */
 public abstract class Persona {
 
-  /**
-   * Nombre completo de la persona.
-   */
-  protected String nombre;
+    protected String nombre;
+    protected String DNI;
+    protected int edad;
+    protected String genero;
 
-  /**
-   * Documento Nacional de Identidad de la persona.
-   */
-  protected String DNI;
+    /**
+     * Constructor de la clase Persona.
+     * @param nombre nombre de la persona
+     * @param DNI documento de identidad
+     * @param edad edad de la persona
+     * @param genero genero de la persona
+     */
+    public Persona(String nombre, String DNI, int edad, String genero) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+        this.edad = edad;
+        this.genero = genero;
+    }
 
-  /**
-   * Edad de la persona en años.
-   */
-  protected int edad;
+    /**
+     * Obtiene el nombre de la persona.
+     * @return nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
-  /**
-   * Género de la persona.
-   */
-  protected String genero;
-
-  /*
-   * @param nombre Nombre completo de la persona.
-   * 
-   * @param DNI Documento Nacional de Identidad.
-   * 
-   * @param edad Edad de la persona.
-   * 
-   * @param genero Género de la persona.
-   */
-  Persona(String nombre, String DNI, int edad, String genero) {
-    this.nombre = nombre;
-    this.DNI = DNI;
-    this.edad = edad;
-    this.genero = genero;
-  }
-
-  /**
-   * Obtiene el nombre de la persona.
-   * 
-   * @return El nombre de la persona.
-   */
-  public String getNombre() {
-    return this.nombre;
-  }
-
-  /**
-   * Método abstracto que debe ser implementado por las clases hijas
-   * para mostrar la información de la persona.
-   * 
-   * Cada subclase define su propia forma de presentar los datos.
-   */
-  public abstract void mostrarDatos();
+    /**
+     * Muestra los datos de la persona.
+     * Método abstracto que debe implementarse en las clases hijas.
+     */
+    public abstract void mostrarDatos();
 }
