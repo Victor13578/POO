93ca1898de0c670d0d49da65a7b224330ca1eee9
juancho-2006 <juans.echeverrif@autonoma.edu.co>
2026-02-25 23:53:55 -0500

    proyecto

---
+++ GestionHospital/src/Cirujano.java
@@ -0,0 +1,24 @@
+public class Cirujano extends Medico implements ICirujano {
+  private int numeroQuirofano;
+  private boolean disponible;
+
+  public Cirujano(String nombre, String DNI, int edad, String genero, String especialidad, int numeroRegistro,
+      int numeroQuirofano, boolean disponible) {
+    super(nombre, DNI, edad, genero, especialidad, numeroRegistro);
+    this.numeroQuirofano = numeroQuirofano;
+    this.disponible = disponible;
+  }
+
+  public boolean validarQuirofano() {
+    return this.disponible;
+  }
+
+  public void realizarOperacion() {
+    if (this.validarQuirofano()) {
+      System.out.println("El cirujano " + this.nombre + " realizó operación en quirófano " + this.numeroQuirofano);
+    } else {
+      System.out.println("El cirujano " + this.nombre + " NO pudo operar. Quirófano ocupado.");
+    }
+
+  }
+}
